451ec019f7d412c6d82039c7c818207666ed6554
lorcevinfinlog-dot <lorcevin.fin.log@gmail.com>
2026-07-01 23:10:43 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker устанавливается');
+const CACHE_NAME = 'calculator-v1';
+const urlsToCache = [
+    '.',
+    'index.html',
+    'manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
 });
